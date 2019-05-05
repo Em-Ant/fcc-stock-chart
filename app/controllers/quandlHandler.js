4fd8196612153c71338926bce34452e55ccdae23
Emanuele Antonelli <joshua999x@gmail.com>
2019-05-06 01:24:19 +0200

    hardcode stock dates

--- app/controllers/quandlHandler.js
+++ app/controllers/quandlHandler.js
@@ -11,9 +11,10 @@ var quandlUrl = 'https://www.quandl.com/api/v3/datasets/WIKI/';
 function StockHandler () {
   this.getStockData = function(req, res) {
     var now = new Date();
-    var month = now.getMonth() + 1;
-    var day = now.getDate();
-    var year = now.getFullYear();
+    // hardcode dates. quandl data are not available anymore after 2018-03-27
+    var month = '03'; //now.getMonth() + 1;
+    var day = '04'; //now.getDate();
+    var year = '2018'; //now.getFullYear();
     var url = quandlUrl
       + req.params.code
       + '.json'
